0e2bef4f25649a0060003098e70e1527a157250d
OozoraCielo <cbragunton@up.edu.ph>
2022-12-12 23:23:24 +0800

    update 1 marker delay problem

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,34 @@
-import {ikotRoutePoints, ikotEveningRoutePoints, tokiRoutePoints} from './jeepRoutes.js'
+import {ikotRoutePoints, ikotEveningRoutePoints, tokiRoutePoints, } from './jeepRoutes.js'
 
 let mapOptions = {
   center:[14.65504,121.06871],
   zoom:15
 }
 
+// setup (1 lang muna ginawa ko sa driverNum)
+let driverNum = 1;
+let coordinates = [];
+let markers = [];//(nakamap dito yung kada isang markers para pwedeng maremove at update)
+for (let i=0; i<driverNum; i++){
+  coordinates[i] = [];
+}
+
 let map = new L.map('map' , mapOptions);
 
-function addMarker(map,coordinates) {
-  let marker = new L.Marker(coordinates);
-  marker.addTo(map);
+function addMarker(map, coordinates, i) {
+  markers[i] = new L.Marker(coordinates);
+  markers[i].addTo(map);
 }
+
+//update lang sa marker, assuming na may bago nang coordinates
+function updateMarker(map, coordinates, i){
+  markers[i].remove(map);
+  console.log(coordinates);
+
+  markers[i] = new L.Marker(coordinates);
+  markers[i].addTo(map);
+}
+
 
 function addRoutes(map) {
   var ikotRoute = L.polyline(ikotRoutePoints, {color: 'yellow'}).addTo(map);
@@ -30,16 +48,29 @@ function displayMap() {
   let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
   map.addLayer(layer);
 
-  let coordinates = [14.65454, 121.06652]
-  addMarker(map, coordinates)
-  coordinates = [14.65496,121.06436]
-  addMarker(map, coordinates)
-  addRoutes(map)
+  //set-up ng initial markers
+  //nakahardcode dito yung coordinates ng marker[0], dapat iba bawat marker gamit yung coordinates[]
+  for (let i=0; i<driverNum; i++){
+    coordinates[0]=ikotEveningRoutePoints[0];
+    console.log(coordinates[0]);
+    console.log(coordinates[0]);
+    addMarker(map, coordinates[i], i);
+  }
+
+  //update ng markers, infinite loop dapat pag may actual coordinates na
+  for (let j=0; j<15; j++){
+    for (let i=0; i<driverNum; i++){
+      setInterval(updateMarker, 1000, map, ikotEveningRoutePoints[j+1], i);//eto yung di ko maintindihan kung panong delay ang gagawin
+    }
+  }
+
+  addRoutes(map);
 
   map.on('click', function(ev){
     var latlng = map.mouseEventToLatLng(ev.originalEvent);
     console.log(latlng.lat + ', ' + latlng.lng);
   });
+
 }
 
 
